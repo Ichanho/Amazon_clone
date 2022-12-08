7fe72fafb1f02ae5d10500e8c41dd7264e8b4bc1
LEE CHAN HO <supprpass15@gmail.com>
2022-12-08 23:56:54 +0900

    Set develop environment by React-Typescript
    With firebase.
    It will be amazon web site (clone)

---
+++ amazon_clone/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCGd4uR9-MOfd4Jx6uVzuULI4oVQYwR2n0",
+  authDomain: "clone-66d13.firebaseapp.com",
+  projectId: "clone-66d13",
+  storageBucket: "clone-66d13.appspot.com",
+  messagingSenderId: "454329336119",
+  appId: "1:454329336119:web:6c4276139831606870dab2",
+  measurementId: "G-R8B4706CV4"
+};
